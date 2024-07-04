cfdf5db88f9272101749015f1d75af131e4af784
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-05 04:59:43 +0530

    terminal working!!!!!

---
+++ server.js
@@ -0,0 +1,37 @@
+//Node js chapter1:
+// How node js is different from vanilla js?
+
+//1. Node js runs on server not in browser (backend not frontend)
+//2. The console is the Terminal now
+console.log("Hello World");
+
+function helloBro() {
+    console.log("hello bro!");
+}
+helloBro();
+
+function aplusb(a,b) {
+    return a + b;
+}
+console.log(aplusb(3,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
