c421fc9321763dbb6e1812c7fbd326a3d461db54
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-05 05:25:24 +0530

    3. global object

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 //1. Node js runs on server not in browser (backend not frontend)
 //2. The console is the Terminal now
+/*
 console.log("Hello World");
 
 function helloBro() {
@@ -13,7 +14,9 @@ helloBro();
 function aplusb(a,b) {
     return a + b;
 }
-console.log(aplusb(3,5));
+console.log(aplusb(3,5)); */
+//3. Here we have golbal object instead of window object ==>> again window object is on browser
+console.log(global);
 
 
 
