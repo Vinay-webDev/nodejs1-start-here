ded4919a25bc0fbe8943eb6f915b4aa79887926f
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-04 21:20:55 +0530

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,30 @@
+//Node js chapter1:
+// how node js is different from vanilla js?
+
+/* 
+1. Node runs on server not in browser (backend not frontend)
+2. The console is The terminal window now */
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
