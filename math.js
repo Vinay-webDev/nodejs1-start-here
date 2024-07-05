9423c336c384c5c03f5eee3801e6b7ea2f9bcea9
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-05 06:26:17 +0530

    ===>> another way export module and 6. difference

--- math.js
+++ math.js
@@ -1,13 +1,19 @@
 //our own module
+/*
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
-const divide = (a,b) => a / b;
+const divide = (a,b) => a / b; 
 
 // 1. way to export 
-module.exports = { add, subtract, multiply, divide };
+module.exports = { add, subtract, multiply, divide }; */
 
+// 2. another way to export these functions
 
+exports.add = (a,b) => a + b;
+exports.subtract = (a,b) => a - b;
+exports.multiply = (a,b) => a * b;
+exports.divide = (a,b) => a / b;
 
 
 
--- server.js
+++ server.js
@@ -58,6 +58,6 @@ console.log(add(5,3));
 console.log(subtract(5,3));
 console.log(multiply(5,3));
 console.log(divide(5,3));
-
+//6. Missing some js APIs like fetch
 
 
